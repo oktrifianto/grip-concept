a406fcae3c0974aaa1386c1fa198ad2323c51c79
Rahmat O <rahmat.oktrifianto@mail.ugm.ac.id>
2020-10-14 16:07:21 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
-/ Create the <h4> element
 var heading = document.createElement("h4"); 
-
-// add the text    
+ 
 heading.textContent = "TEST";
-
-// Create the text node <p>       
+    
 var p = document.createTextNode("blablabla");  
 
-//get the element to which you want to append the h4 and p
 var container = document.getElementById("myID");
 
-// Append h4 and p 
 container.appendChild(heading);
 container.appendChild(p);   
